333102aa31ffe90f8ce3512a72b236c42176aa0d
stop52 <stop52@naver.com>
2023-06-13 18:45:03 +0900

    Add video encryption and decryption in client side

--- LgVideoChatDemo/LgVideoChatDemo/Crypto.h
+++ LgVideoChatDemo/LgVideoChatDemo/Crypto.h
@@ -3,6 +3,6 @@
 void CryptoInitialize();
 void CryptoTest();
 bool AesEncryptForSend(const unsigned char* msg, size_t msg_len,
-    unsigned char* encrypted_msg, size_t* encrypted_msg_len);
+    std::vector<unsigned char>* encrypted_msg, size_t* encrypted_msg_len);
 bool AesDecryptForRecieve(const unsigned char* msg, size_t msg_len,
     std::vector<unsigned char>* decrypted_msg, size_t* decrypted_msg_len);
